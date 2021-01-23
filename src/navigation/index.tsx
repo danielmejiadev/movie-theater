6ae652b47584d6d91c01dc5ee10e3116f49bed3c
Daniel Mejia <luisdanielmejia@outlook.com>
2021-01-23 12:07:51 -0500

    Add Navigation component

---
+++ src/navigation/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+// Pages
+import MovieDetail from '../pages/MovieDetail';
+import Home from '../pages/Home';
+
+function Navigation() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/movieDetail">
+          <MovieDetail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default Navigation;
