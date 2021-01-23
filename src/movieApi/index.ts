fb6140dfa5366321362ca6266e36e2f1361a6887
Daniel Mejia <luisdanielmejia@outlook.com>
2021-01-23 12:09:02 -0500

    Integrate react env variables to load values

---
+++ src/movieApi/index.ts
@@ -0,0 +1,17 @@
+import { AxiosRequestConfig } from 'axios';
+import MovieApi from './movieApi';
+
+const { REACT_APP_BASE_URL } = process.env;
+
+function configureGlobalAuthToken(config: AxiosRequestConfig) {
+  const { REACT_APP_TOKEN_MOVIE } = process.env;
+  config.headers = { 
+    Authorization: `Bearer ${REACT_APP_TOKEN_MOVIE}`,
+  }
+  return config;
+}
+
+const movieApi = new MovieApi(REACT_APP_BASE_URL!)
+  .addRequestInterceptor(configureGlobalAuthToken);
+
+export default movieApi;
