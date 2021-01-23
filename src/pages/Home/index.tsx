53f5e0d474c1e8b807a0ba8be563d47d9ff1535c
Daniel Mejia <luisdanielmejia@outlook.com>
2021-01-23 13:22:49 -0500

    Add list of movies

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React from "react";
+import { States, useFetch } from "../../hooks/useFetch";
+import movieApi from "../../movieApi";
+import MovieCard from "../../components/MovieCard";
+import { GridContainer } from "./styles";
 
 function Home() {
-  return <h2>Home</h2>;
+  const [data, state] = useFetch(() => movieApi.discover.popularMovies(), {});
+
+  if ([States.IDLE, States.LOADING].includes(state)) {
+    return <p>Loading</p>;
+  }
+
+  return (
+    <GridContainer>
+      {data.results?.map((movie) => (
+        <MovieCard
+          key={movie.id}
+          title={movie.title}
+          imagePath={movie.poster_path}
+          releaseDate={movie.release_date}
+        />
+      ))}
+    </GridContainer>
+  );
 }
 
 export default Home;
