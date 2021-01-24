a6ad921832a20cc16be5c7170aa20cddba99e196
Daniel Mejia <luisdanielmejia@outlook.com>
2021-01-24 16:13:16 -0500

    Add the filter movies api service

--- src/movieApi/resources/search.ts
+++ src/movieApi/resources/search.ts
@@ -1,5 +1,6 @@
 // Depedencies
 import Base from './base';
+import { MoviesResults } from '../../interfaces/MovieResults';
 
 /**
  * Search resource.
@@ -12,6 +13,21 @@ export class Search extends Base {
    * @type { string }
    */
   resourceUrl = 'search';
+
+  /**
+   * Filter movies by given query text.
+   * @param {string} query The query text to filter.
+   * @returns {Promise<MoviesResults>} The movie results.
+   */
+  filterMovies(query: string): Promise<MoviesResults> {
+    const params = new URLSearchParams();
+    params.append('query', query);
+
+    return this.list<MoviesResults>({
+      url: `${this.resourceUrl}/movie`,
+      params
+    });
+  }
 }
 
 export default Search;
