88a5b193cc1ec82af4049266a595da05965d83c7
Daniel Mejia <luisdanielmejia@outlook.com>
2021-01-24 16:50:40 -0500

    Wrap the app using the MovieProvider

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 import './App.css';
 
 import Navigation from './navigation';
+import MovieProvider from './providers/MovieProvider';
 
 function App(): JSX.Element {
-  return <Navigation />;
+  return (
+    <MovieProvider>
+      <Navigation />;
+    </MovieProvider>
+  );
 }
 
 export default App;
