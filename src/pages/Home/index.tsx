d8c6d481962c8f300382a78314a29127978fd507
Daniel Mejia <luisdanielmejia@outlook.com>
2021-01-25 17:48:47 -0500

    Integrate Search and Rating to Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,31 +1,39 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import { States } from '../../hooks/useFetch';
-import MovieCard from '../../components/MovieCard';
-import Header from '../../components/Header';
-import { GridContainer } from './styles';
+import { Wrapper } from './styles';
 import { useMovie } from '../../hooks/useMovie';
+import { Movie } from '../../interfaces/Movie';
+import Header from '../../components/Header';
+import SearchResults from '../../components/SearchResults';
+import MovieList from '../../components/MovieList';
+import Suspense from '../../components/Suspense';
 
 function Home(): JSX.Element {
-  const { movieResults, state } = useMovie();
   const history = useHistory();
+  const goToMovieDetail = ({ id }: Movie) => history.push(`movieDetail/${id}`);
+  const {
+    movieResults: { results },
+    state,
+    query
+  } = useMovie();
 
   return (
     <div>
       <Header />
-      {[States.IDLE, States.LOADING].includes(state) && <p>Loading</p>}
-      <GridContainer>
-        {movieResults.results?.map((movie) => (
-          <MovieCard
-            key={movie.id}
-            onClick={() => history.push(`movieDetail/${movie.id}`)}
-            title={movie.title}
-            imagePath={movie.poster_path}
-            releaseDate={movie.release_date}
-            voteAverage={movie.vote_average}
-          />
-        ))}
-      </GridContainer>
+      <Wrapper>
+        <Suspense state={state}>
+          {query ? (
+            <SearchResults onMovieClicked={goToMovieDetail} />
+          ) : (
+            <MovieList
+              header={<h2>Most popular</h2>}
+              movies={results}
+              onMovieClicked={goToMovieDetail}
+              emptyComponent={<h3>There are no popular movies</h3>}
+            />
+          )}
+        </Suspense>
+      </Wrapper>
     </div>
   );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,8 +1,5 @@
 import styled from 'styled-components';
 
-export const GridContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(180px, 1fr));
-  grid-gap: 20px;
-  padding: 60px;
+export const Wrapper = styled.div`
+  padding: 40px 60px;
 `;
